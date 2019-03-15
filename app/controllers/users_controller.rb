5d967904a6318a057c99c296fb6c3a6b53a96f09
yokoyama <startnow7240@gmail.com>
2019-03-15 19:49:02 +0900

    課題05提出用

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,21 @@
 class UsersController < ApplicationController
-validates :password, length: { in: 8..32 }
-validates :email, presence: true, format: {with: /\w+@\w+\.\w+/}
-validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i}
-validates :name, presence: true, length: { maximum: 15 }
-end
   def new
     @user = User.new
   end
 
   def create
 
-  @user = User.new(user_params)
-  if @user.save
-    redirect_to root_path, success: '登録が完了しました'
-  else
-    flash.now[:danger] = "登録に失敗しました"
-    render :new
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, success: '登録が完了しました'
+    else
+      flash.now[:danger] = "登録に失敗しました"
+      render :new
+    end
   end
-end
 
-private
- def user_params
+  private
+  def user_params
    params.require(:user).permit(:name, :email, :password, :password_confirmation)
- end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
-
+  validates :password, length: { in: 8..32 }
+  validates :email, format: {with: /\A[\w+\-~.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i}
+  validates :name, presence: true, length: { maximum: 15 }
   has_secure_password
 end
