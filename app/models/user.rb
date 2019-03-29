589408ddb5149fe68007d943531db1de36b93e30
yokoyama <startnow7240@gmail.com>
2019-03-30 00:07:02 +0900

    課題07修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i}
   has_secure_password
 
   has_many :topics
+  has_many :favorites
+  has_many :favorite_topics, through: :favorites, source: 'topic'
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,9 @@ Rails.application.routes.draw do
 
   resources :users
   resources :topics
+
+  get 'favorites/index'
+ post '/favorites', to: 'favorites#create'
+ 
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
